bda54b97c801ec2022edcdf0e7c16ca01d83d0ee
Max Desiatov <max@desiatov.com>
2020-03-16 18:30:19 +0000

    Update match.js

--- match.js
+++ match.js
@@ -12,8 +12,8 @@ function parseAllowed(allowed) {
 function findMatching(labelNames, allowedLabels) {
   const allowedLabelsSet = new Set(allowedLabels)
   const matchingLabels = labelNames.filter(labelName => allowedLabelsSet.has(labelName))
-  if (matchingLabels.length != 1) {
-    throw new Error('Could not find exactly one of the appropriate labels on the PR.')
+  if (matchingLabels.length < 1) {
+    throw new Error('Could not find one of the appropriate labels on the PR.')
   }
   return matchingLabels[0]
 }
