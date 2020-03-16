a265d8802d973956a10fec517a3c5833b090ffa0
Max Desiatov <max@desiatov.com>
2020-03-16 18:56:52 +0000

    Rebuild with verbose error message

--- match.js
+++ match.js
@@ -13,7 +13,10 @@ function findMatching(labelNames, allowedLabels) {
   const allowedLabelsSet = new Set(allowedLabels)
   const matchingLabels = labelNames.filter(labelName => allowedLabelsSet.has(labelName))
   if (matchingLabels.length < 1) {
-    throw new Error('Could not find one of the appropriate labels on the PR.')
+    throw new Error(
+      `Could not find one of the appropriate labels on the PR. 
+      Allowed labels: ${allowedLabels}
+      Present labels: ${labelNames}`)
   }
   return matchingLabels[0]
 }
